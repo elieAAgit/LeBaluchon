3c44430ed46e28520175a259381c00ea9a920c8c
Elie <milhouze@live.fr>
2020-02-01 20:45:38 +0100

    Creation of a translation scene in Main.Storyboard, with scene design. Creation of an empty TranslationViewController in Controller related to translation scene in Main.Storyboard.

---
+++ LeBaluchon/Controller/TranslationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TranslationViewController.swift
+//  LeBaluchon
+//
+//  Created by Elie Arquier on 01/02/2020.
+//  Copyright © 2020 Elie. All rights reserved.
+//
+
+import UIKit
+
+class TranslationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
