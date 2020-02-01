9d43118abf8d4ada6f1a5e51891912d0ff73e7b8
Elie <milhouze@live.fr>
2020-02-01 09:43:04 +0100

    Creation of a change scene in Main.Storyboard, with scene design. Creation of an empty ChangeViewController in Controller related to a change scene in Main.Storyboard.

---
+++ LeBaluchon/Controller/ChangeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChangeViewController.swift
+//  LeBaluchon
+//
+//  Created by Elie Arquier on 30/01/2020.
+//  Copyright © 2020 Elie. All rights reserved.
+//
+
+import UIKit
+
+class ChangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
