f6657702491402abeac9f5f91db37bad099a291b
Elie <milhouze@live.fr>
2020-02-06 22:33:30 +0100

    TranslationViewController: Adaptive view depending on the keyboard using specific keyboard notification with UIResponder. Using UITextFieldDelegate for keyboard management.
    Update Main.storyboard.

--- LeBaluchon/Controller/TranslationViewController.swift
+++ LeBaluchon/Controller/TranslationViewController.swift
@@ -9,22 +9,71 @@
 import UIKit
 
 class TranslationViewController: UIViewController {
-
+    @IBOutlet weak var languageToTranslateTtextField: UITextField!
+    @IBOutlet weak var languageTraductedTextView: UITextView!
+    
+    @IBOutlet weak var translationMainStackView: UIStackView!
+    @IBOutlet weak var topMainStackConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomMainStackConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        languageToTranslateTtextField.delegate = self
+
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
-    */
+}
 
+// MARK: - Keyboard control
+extension TranslationViewController: UITextFieldDelegate {
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        hideKeyboard()
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+
+        return true
+    }
+
+    private func hideKeyboard() {
+        languageToTranslateTtextField.resignFirstResponder()
+    }
+}
+
+// MARK: - Adaptive view depending on the keyboard
+extension TranslationViewController {
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {  return }
+
+        if notification.name == UIResponder.keyboardWillShowNotification ||
+           notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            view.frame.origin.y = -keyboardRect.height/2
+
+            UIView.animate(withDuration: 0.8) {
+                self.topMainStackConstraint.constant = keyboardRect.height/2
+                self.bottomMainStackConstraint.constant = keyboardRect.height/2 - 45
+                self.translationMainStackView.layoutIfNeeded()
+            }
+        } else {
+            view.frame.origin.y = 0
+            
+            UIView.animate(withDuration: 0.8) {
+                self.topMainStackConstraint.constant = 20
+                self.bottomMainStackConstraint.constant = 20
+                self.translationMainStackView.layoutIfNeeded()
+            }
+        }
+    }
 }
